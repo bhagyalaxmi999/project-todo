f18e04bd1e28b547ac1bdcde1217c6174cb97cb2
bhagyalaxmi999 <ch.anushaa9@gmail.com>
2023-04-10 18:46:57 +0530

    updated the components

---
+++ src/component/Create.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+
+// state is declare internal component =useState
+// to read value from input in react froms
+// mutable => useRef() book
+// immutable => state and onChange method
+
+function Create(props) {
+    // const [state,handler()] = useState(initialValue)
+    const [fname,setFName] = useState('')
+    const [femail,setFEmail] = useState('')
+    const [fmobile,setFMobile] = useState('')
+    const [fimage,setFImage] = useState('')
+    const [faddress,setfAddress] = useState('')
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 text-center">
+                    <h3 className="display-4 text-success">Create new</h3>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <form autoComplete="off">
+                                <div className="form-group mt-2">
+                                    <label htmlFor="name">Name</label>
+                                    <input type="text" name="name" id="name" value={fname} onChange={(e) => setFName(e.target.value)} className="form-control" required />
+
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" name="email" id="email" value={femail} onChange={(e) => setFEmail(e.target.value)} className="form-control" required />
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor="mobile">Mobile</label>
+                                    <input type="number" name="mobile" id="mobile" value={fmobile} onChange={(e) => setFMobile(e.target.value)} className="form-control" required />
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor="image">Image</label>
+                                    <input type="url" name="image" id="image" value={fimage} className="form-control" required />
+                                </div>
+
+                                <div className="form-group mt-2">
+                                    <label htmlFor="address">Address</label>
+                                    <textarea name="address" id="address" cols="30" rows="5" className="form-control" required></textarea>
+                                </div>
+                                <div className="form-group mt-2">
+                                    <input type="submit" value="Create" className="btn btn-outline-success" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Create;
